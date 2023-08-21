d4ce12e78992ac7c3f291e88bbce2767212f43a8
amer-adam <ameradam6a@gmail.com>
2023-08-21 10:18:55 +0800

    first commit

---
+++ pwm-motor.c
@@ -0,0 +1,430 @@
+#include "pwm-motor.h"
+
+void getDistance(void)
+{
+
+    /* pull trigger pin HIGH */
+    // char current;
+    // char count;
+    TRIGGER_PIN_ON;
+
+    /* provide 10uS Delay*/
+    TL1 = 0xF5;
+    TH1 = 0xFF;
+    TR1 = 1;
+    while (TF1 == 0)
+        ;
+    TR1 = 0;
+    TF1 = 0;
+
+    /* pull trigger pin LOW*/
+    TRIGGER_PIN_OFF;
+
+    while (!ECHO_PIN_CHECK)
+        ;    /* Waiting for Echo */
+    TR1 = 1; /* Timer Starts */
+    while (ECHO_PIN_CHECK && !TF1)
+        ;
+    TR1 = 0; /* Stop the timer */
+    /* calculate distance using timer */
+    distance = ((TL1 | (TH1 << 8)) * Clock_period * sound_velocity) / 2;
+} /* Waiting for Echo goes LOW */
+
+/************************************************
+ * Initiliaze the Board
+ ************************************************/
+void boardInit(void)
+{
+    TimerInit(); // Initialize timer to start generating interrupts
+
+    P0 = 0xFF;
+    P1 = 0xFF;
+    P2 = 0xFF;
+    P3 = 0xFF;
+
+    autoflag = 0;
+    sysTick = 0;
+    ultrasonic_timer = 0;
+    pwm_left = 0;
+    pwm_right = 0;
+    speedL = 0;
+    speedR = 0;
+}
+
+/************************************************
+ * Initiliaze the Timers
+ ************************************************/
+void TimerInit(void)
+{
+    // Set Timer 0 mode to 16-bit
+    TMOD &= 0x00; // Clear lower 4 bits
+    TMOD |= 0x11; // Set the first bit to configure Timer 0 as a 16-bit timer (Mode 1)
+
+    // Set initial values for Timer 0
+    TL0 = 0xFA; // Low byte initial value
+    TH0 = 0xFF; // High byte initial value
+
+    // Enable Timer 0 interrupt and global interrupt
+    ET0 = 1; // Enable Timer 0 interrupt
+    EA = 1;  // Enable global interrupts
+
+    IT1 = 1; // Set INT1 to be edge-triggered (rising edge)
+    EX1 = 1; // Enable External Interrupt 1
+
+    // Start Timer 0
+    TR0 = 1; // Start Timer 0 by setting its run control bit
+}
+
+/************************************************
+ * Timer 0 Callback
+ * Used to generate pwm signals
+ ************************************************/
+void InterruptTimer0() __interrupt(1)
+{
+    TR0 = 0; // Stop Timer 0
+
+    TL0 = 0xFC; // Load low byte of timer value
+    TH0 = 0xFF; // Load high byte of timer value
+
+    pwm_left++;  // Increment pwm_left counter
+    pwm_right++; // Increment pwm_right counter
+    sysTick++;   // Increment sysTick counter
+
+    if (pwm_left >= 100) // Check if pwm_left has reached 100
+    {
+        pwm_left = 0; // Reset pwm_left counter to 0
+    }
+
+    if (pwm_left <= speedL)
+    {
+        EN1_ON;
+    }
+    else
+    {
+        EN1_OFF;
+    }
+
+    if (pwm_right >= 100) // Check if pwm_right has reached 100
+    {
+        pwm_right = 0; // Reset pwm_right counter to 0
+    }
+
+    if (pwm_right <= speedR)
+    {
+        EN2_ON;
+    }
+    else
+    {
+        EN2_OFF;
+    }
+
+    if (sysTick - ultrasonic_timer >= 5000) // 2HZ timer
+    {
+        ultrasonic_timer = sysTick;
+        ultrasonic_flag = 1;
+    }
+
+    TR0 = 1; // Resume Timer 0
+}
+
+/************************************************
+ * External Interrupt 1 (INT1) handler
+ * Used to handle ultrasonic callback
+ ************************************************/
+void External1_ISR() __interrupt(2)
+{
+}
+
+/************************************************
+ * Move the car forward
+ * Minimum speed
+ ************************************************/
+void forward(void)
+{
+    speedL = 25;
+    speedR = 25;
+    MOTOR_L_GO;
+    MOTOR_R_GO;
+}
+
+/************************************************
+ * Stop the car
+ ************************************************/
+void stop(void)
+{
+    speedL = 0;
+    speedR = 0;
+    MOTOR_L_STOP;
+    MOTOR_R_STOP;
+}
+
+/************************************************
+ * SPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED
+ ************************************************/
+void nitro(void)
+{
+    speedL = 80;
+    speedR = 80;
+    MOTOR_L_GO;
+    MOTOR_R_GO;
+}
+
+/************************************************
+ * Turn the robot fast to the right or left
+ ************************************************/
+void turnFast(CAR_DIRECTION_t d)
+{
+    speedL = 60;
+    speedR = 60;
+    if (d == RIGHT)
+    {
+        MOTOR_L_BACK;
+        MOTOR_R_GO;
+    }
+    else if (d == LEFT)
+    {
+        MOTOR_L_GO;
+        MOTOR_R_BACK;
+    }
+}
+
+/************************************************
+ * Turn the robot slowly to the right or left
+ ************************************************/
+void turnSlow(CAR_DIRECTION_t d)
+{
+
+    if (d == RIGHT)
+    {
+        speedL = 30;
+        speedR = 90;
+        MOTOR_L_BACK;
+        MOTOR_R_GO;
+    }
+    else if (d == LEFT)
+    {
+        speedL = 90;
+        speedR = 30;
+        MOTOR_L_GO;
+        MOTOR_R_BACK;
+    }
+}
+
+/************************************************
+ * Delay for some milliseconds
+ ************************************************/
+void delayMs(unsigned int i)
+{
+    unsigned int j;
+    while (i--)
+    {
+        for (j = 0; j < 125; j++)
+            ;
+    }
+}
+
+/************************************************
+ * Check IR Sensors and toggle leds
+ * Must be called in the main while loop
+ ************************************************/
+void checkSensor(void)
+{
+    if (IR1)
+    {
+        bitclear(P1, 0);
+    }
+    else
+    {
+        bitset(P1, 0);
+    }
+    if (IR2)
+    {
+        bitclear(P1, 1);
+    }
+    else
+    {
+        bitset(P1, 1);
+    }
+    if (IR3)
+    {
+        bitclear(P1, 2);
+    }
+    else
+    {
+        bitset(P1, 2);
+    }
+    if (IR4)
+    {
+        bitclear(P1, 3);
+    }
+    else
+    {
+        bitset(P1, 3);
+    }
+}
+
+/************************************************
+ * Main line following algorithem
+ * Must be called in the main while loop
+ ************************************************/
+void lineFollow(void)
+{
+    if (!IR1 && !IR2 && !IR3 && !IR4) // 0000
+    {
+        forward();
+    }
+
+    if (!IR1 && !IR2 && IR3 && !IR4) // 0010
+    {
+        turnSlow(RIGHT);
+    }
+
+    if (!IR1 && !IR2 && !IR3 && IR4) // 0001
+    {
+        delayMs(6);
+
+        do
+        {
+            turnFast(RIGHT);
+        } while (!IR2);
+
+        do
+        {
+            turnSlow(LEFT);
+        } while (!IR3);
+    }
+
+    if (!IR1 && !IR2 && IR3 && IR4) // 0011
+    {
+        do
+        {
+            turnSlow(RIGHT);
+        } while (!IR2);
+    }
+
+    // if (!IR1 && IR2 && !IR3 && IR4) // 0101
+    // {
+    //     delayMs(610);
+
+    //     do
+    //     {
+    //         turnSlow(RIGHT);
+    //     } while (!IR1);
+    // }
+
+    if (!IR1 && IR2 && !IR3 && !IR4) // 0100
+    {
+        turnSlow(LEFT);
+    }
+
+    if (IR1 && !IR2 && !IR3 && !IR4) // 1000
+    {
+        delayMs(6);
+
+        do
+        {
+            turnFast(LEFT);
+        } while (!IR3);
+
+        do
+        {
+            turnSlow(RIGHT);
+        } while (!IR2);
+    }
+    if (IR1 && IR2 && !IR3 && !IR4) // 1100
+    {
+        do
+        {
+            turnSlow(LEFT);
+        } while (!IR3);
+    }
+    // if (IR1 && !IR2 && IR3 && !IR4) // 1010
+    // {
+    //     delayMs(100);
+
+    //     do
+    //     {
+    //         turnSlow(LEFT);
+    //     } while (!IR4);
+    // }
+    if (IR1 && IR2 && IR3 && IR4) // 1111
+    {
+        stop();
+    }
+}
+
+/************************************************
+ * Push Buttons controll
+ * Must be called in the main while loop
+ ************************************************/
+void pushButton(void)
+{
+    if (!K1)
+    {
+        while (!K1)
+            ;
+        autoflag = 1;
+    }
+    if (!K2)
+    {
+        while (!K2)
+            ;
+        autoflag = 0;
+    }
+    // if (!K3)
+    // {
+    //     while (!K3)
+    //         ;
+    //     stop();
+    //     autoflag = 0;
+    // }
+}
+
+// void DisplayNumber()
+// {
+//     // char buf[4];
+//     float n = 153.4;
+//     P2_3 = 0;
+//     P0 = num[1];
+//     P2_3 = 1;
+//     P2_0 = 0;
+//     P0 = num[2];
+//     P2_0 = 1;
+//     P2_1 = 0;
+//     P0 = num[3];
+//     P2_1 = 1;
+//     P2_2 = 0;
+//     P0 = num[4];
+//     P2_2 = 1;
+// }
+
+int main(void)
+{
+    boardInit();
+
+    while (1)
+    {
+        if (autoflag)
+            lineFollow();
+        else
+            stop();
+
+        if (ultrasonic_flag)
+        {
+            ultrasonic_flag = 0;
+            getDistance();
+        }
+
+        if (distance <= 10)
+        {
+            // stop();
+            bitclear(P1, 7);
+        }
+        else
+        {
+            bitset(P1, 7);
+        }
+        checkSensor();
+        pushButton();
+    }
+}
---
+++ pwm-motor.h
@@ -0,0 +1,154 @@
+#ifndef PWM_MOTOR_H
+#define PWM_MOTOR_H
+
+/************************************************
+ * includes
+ ************************************************/
+#include <8051.h>
+
+/************************************************
+ * useful macros
+ ************************************************/
+#define bitset(byte, nbit) ((byte) |= (1 << (nbit)))
+#define bitclear(byte, nbit) ((byte) &= ~(1 << (nbit)))
+#define bitcheck(byte, nbit) ((byte) & (1 << (nbit)))
+#define bitflip(byte, nbit) ((byte) ^= (1 << (nbit)))
+
+/************************************************
+ * GPIO PINS
+ ************************************************/
+// #define K4 P3_2   // button 1
+// #define K3 P3_3   // button 2
+#define K2 P3_4   // button 3
+#define K1 P3_5   // button 4
+#define BEEP P3_6 // buzzer
+
+#define IN1 7
+#define IN2 2
+#define IN3 1
+#define IN4 0
+#define EN1 4
+#define EN2 5
+
+/************************************************
+ * motor enable pins control
+ ************************************************/
+#define EN1_Check bitcheck(P2, EN1)
+#define EN1_ON bitset(P2, EN1)
+#define EN1_OFF bitclear(P2, EN1)
+
+#define EN2_Check bitcheck(P2, EN2)
+#define EN2_ON bitset(P2, EN2)
+#define EN2_OFF bitclear(P2, EN2)
+
+/************************************************
+ * motor direction pins control
+ ************************************************/
+#define MOTOR_L_GO                          \
+    {                                       \
+        bitclear(P3, IN1), bitset(P3, IN2); \
+    }
+
+#define MOTOR_L_BACK                        \
+    {                                       \
+        bitset(P3, IN1), bitclear(P3, IN2); \
+    }
+
+#define MOTOR_R_GO                          \
+    {                                       \
+        bitclear(P3, IN3), bitset(P3, IN4); \
+    }
+
+#define MOTOR_R_BACK                        \
+    {                                       \
+        bitset(P3, IN3), bitclear(P3, IN4); \
+    }
+
+#define MOTOR_L_STOP                          \
+    {                                         \
+        bitclear(P3, IN1), bitclear(P3, IN2); \
+    }
+
+#define MOTOR_R_STOP                          \
+    {                                         \
+        bitclear(P3, IN3), bitclear(P3, IN4); \
+    }
+
+/************************************************
+ * sensor pins
+ ************************************************/
+#define IR1 bitcheck(P0, 0) // ir sensor 1 is connected to pin P00
+#define IR2 bitcheck(P0, 1) // ir sensor 2 is connected to pin P01
+#define IR3 bitcheck(P0, 2) // ir sensor 3 is connected to pin P02
+#define IR4 bitcheck(P0, 3) // ir sensor 4 is connected to pin P03
+
+/************************************************
+ * ultrasonic pins control
+ *
+ ************************************************/
+#define TRIGGER_PIN_ON bitset(P2, 6)
+#define TRIGGER_PIN_OFF bitclear(P2, 6)
+#define ECHO_PIN_CHECK bitcheck(P3, 3)
+
+/************************************************
+ * Global variables
+ ************************************************/
+#define sound_velocity 34300 /* sound velocity in cm per second */
+#define period_in_us 0.000001
+#define Clock_period 1.085 * period_in_us /* period for clock cycle of 8051*/
+
+unsigned char speedR; // It can have a value from 0 (0% duty cycle) to 100 (100% duty cycle)
+unsigned char speedL; // It can have a value from 0 (0% duty cycle) to 100 (100% duty cycle)
+float distance;
+
+unsigned char pwm_left;
+unsigned char pwm_right;
+unsigned int sysTick;
+unsigned int ultrasonic_timer;
+
+unsigned short ultrasonic_flag;
+unsigned short autoflag;
+
+typedef enum
+{
+    LEFT,
+    RIGHT
+} CAR_DIRECTION_t;
+
+typedef union
+{
+    unsigned char flags;
+    struct
+    {
+        unsigned char sent_trigger : 1;
+        unsigned char started_timer : 1;
+        unsigned char finished_timer : 1;
+        unsigned char bit4 : 1;
+        unsigned char bit5 : 1;
+        unsigned char bit6 : 1;
+        unsigned char bit7 : 1;
+        unsigned char bit8 : 1;
+    } flag;
+} sys_t;
+
+sys_t sys;
+
+unsigned char num[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82,
+                       0xf8, 0x80, 0x90};
+
+/************************************************
+ * Function Prototypes
+ ************************************************/
+void getDistance(void);
+void boardInit(void);
+void TimerInit(void);
+void forward(void);
+void stop(void);
+void nitro(void);
+void turnFast(CAR_DIRECTION_t d);
+void turnSlow(CAR_DIRECTION_t d);
+void delayMs(unsigned int i);
+void checkSensor(void);
+void lineFollow(void);
+
+#endif // PWM_MOTOR_H
